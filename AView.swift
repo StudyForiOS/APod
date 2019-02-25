9325d53299c5df57d82af46b2cbb4ca940776b8c
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-25 11:17:46 +0800

    0.1.1

---
+++ AView.swift
@@ -0,0 +1,20 @@
+//
+//  AView.swift
+//  APod
+//
+//  Created by ForJade Giant on 2019/2/25.
+//
+
+import UIKit
+
+class AView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
