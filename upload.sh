72e73e68658e0c26d934122dd6b746baf824175f
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-25 11:21:24 +0800

    add ../APod/Classes/AView.swift

---
+++ upload.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git stash
+git pull origin master --tags
+git stash pop
+
+VersionString=`grep -E 's.version.*=' APod.podspec`
+VersionNumber=`tr -cd 0-9 <<<"$VersionString"`
+#NewVersionNumber=$(($VersionNumber + 2))
+NewVersionNumber=0.1.2
+LineNumber=`grep -nE 's.version.*=' APod.podspec | cut -d : -f1`
+sed -i "" "${LineNumber}s/${VersionNumber}/${NewVersionNumber}/g" BaseBusiness.podspec
+
+echo "current version is ${VersionNumber}, new version is ${NewVersionNumber}"
+
+
+git commit -am ${NewVersionNumber}
+git tag ${NewVersionNumber}
+git push origin master --tags
+pod trunk push ./APod.podspec --verbose --use-libraries --allow-warnings --use-modular-headers
