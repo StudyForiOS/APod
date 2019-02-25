35dfc8bc1050d19e08446296ba9d6b02d7f3a600
zgy朱光阳 <gy.zhu@ctrip.com>
2019-02-25 11:50:41 +0800

    Rename

--- Example/Pods/Target Support Files/Pods-GYAPod_Example/Pods-GYAPod_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-GYAPod_Example/Pods-GYAPod_Example-frameworks.sh
@@ -143,9 +143,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BaseBusiness/BaseBusiness.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GYAPod/GYAPod.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "${BUILT_PRODUCTS_DIR}/BaseBusiness/BaseBusiness.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/GYAPod/GYAPod.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
